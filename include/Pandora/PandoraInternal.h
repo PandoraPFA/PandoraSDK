28c39fde2737c5158c32dd3f974573073ab7941f
marshall <marshall@e861a63a-b6b5-47dc-a5de-b4304083765c>
2015-02-12 16:46:00 +0000

    Support c++11 (and 98/03, with reduced functionality).
    git-svn-id: http://svnsrv.desy.de/public/PandoraPFANew/PandoraSDK/trunk@1709 e861a63a-b6b5-47dc-a5de-b4304083765c

--- include/Pandora/PandoraInternal.h
+++ include/Pandora/PandoraInternal.h
@@ -13,8 +13,14 @@
 #include <set>
 #include <sstream>
 #include <string>
-#include <unordered_set>
 #include <vector>
+
+#if __cplusplus > 199711L
+#include <unordered_set>
+#define MANAGED_CONTAINER unordered_set
+#else
+#define MANAGED_CONTAINER set
+#endif
 
 #include <stdint.h>
 
@@ -148,14 +154,14 @@ inline bool PointerLessThan<T>::operator()(const T *lhs, const T *rhs) const
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-typedef std::unordered_set<const CaloHit *> CaloHitList;
-typedef std::unordered_set<const Cluster *> ClusterList;
-typedef std::unordered_set<const DetectorGap *> DetectorGapList;
-typedef std::unordered_set<const MCParticle *> MCParticleList;
-typedef std::unordered_set<const ParticleFlowObject *> ParticleFlowObjectList;
-typedef std::unordered_set<const ParticleFlowObject *> PfoList;
-typedef std::unordered_set<const Track *> TrackList;
-typedef std::unordered_set<const Vertex *> VertexList;
+typedef std::MANAGED_CONTAINER<const CaloHit *> CaloHitList;
+typedef std::MANAGED_CONTAINER<const Cluster *> ClusterList;
+typedef std::MANAGED_CONTAINER<const DetectorGap *> DetectorGapList;
+typedef std::MANAGED_CONTAINER<const MCParticle *> MCParticleList;
+typedef std::MANAGED_CONTAINER<const ParticleFlowObject *> ParticleFlowObjectList;
+typedef std::MANAGED_CONTAINER<const ParticleFlowObject *> PfoList;
+typedef std::MANAGED_CONTAINER<const Track *> TrackList;
+typedef std::MANAGED_CONTAINER<const Vertex *> VertexList;
 
 typedef std::vector<const CaloHit *> CaloHitVector;
 typedef std::vector<const Cluster *> ClusterVector;
