24da105a6d7df5ac37302e6f956b7fdc935b4142
John Marshall <John.Marshall@warwick.ac.uk>
2025-02-11 11:34:41 +0000

    MCParticleWeights are now of type pandora::InputFloat to guard against invalid inputs.

--- include/Api/PandoraApiImpl.h
+++ include/Api/PandoraApiImpl.h
@@ -110,7 +110,7 @@ private:
      *  @param  mcParticleWeight weighting to assign to the mc particle
      */
     StatusCode SetCaloHitToMCParticleRelationship(
-        const void *const pCaloHitParentAddress, const void *const pMCParticleParentAddress, const float mcParticleWeight) const;
+        const void *const pCaloHitParentAddress, const void *const pMCParticleParentAddress, const InputFloat mcParticleWeight) const;
 
     /**
      *  @brief  Set track to mc particle relationship
@@ -120,7 +120,7 @@ private:
      *  @param  mcParticleWeight weighting to assign to the mc particle
      */
     StatusCode SetTrackToMCParticleRelationship(
-        const void *const pTrackParentAddress, const void *const pMCParticleParentAddress, const float mcParticleWeight) const;
+        const void *const pTrackParentAddress, const void *const pMCParticleParentAddress, const InputFloat mcParticleWeight) const;
 
     /**
      *  @brief  Get the current pfo list
